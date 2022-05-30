5cadbd7b384f1d8c1d02008e87f57a154eb5cd02
Tebello Thejane <zyxoas@gmail.com>
2022-05-30 17:56:10 +0200

    Fix formatting - tabs to spaces

--- robot_apocalypse/src/main/java/com/ioco/bots/tebello/robot_apocalypse/RobotApocalypseApplication.java
+++ robot_apocalypse/src/main/java/com/ioco/bots/tebello/robot_apocalypse/RobotApocalypseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RobotApocalypseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RobotApocalypseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RobotApocalypseApplication.class, args);
+    }
 
 }
